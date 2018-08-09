78b750901fd12c2b3e7425ad04fd65634b2fa00a
Axel D'Olislager <axel.dolislager@gmail.com>
2018-08-10 00:59:29 +0200

    Added passport-local auth
    added auth using mongodb and passport-local

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express')
-
 const app = express()
+const port = process.env.PORT || 3000;
+
+const mongoose = require('mongoose')
+const configDB = require('./config/database.js')
+mongoose.connect(configDB.url, {useMongoClient: true})
+
+const passport = require('passport')
+require('./config/passport')(passport)
+const flash = require('connect-flash')
+
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+
+
+// Express
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(bodyParser())
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.set('views', './views')
 
-require('./routes/routes.js')(app);
+// Passport
+app.use(session({ secret: 'shhitsadamnsecretmate' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-app.listen(3000)
+// Routes
+require('./app/routes.js')(app, passport);
+
+// Launch
+app.listen(port)
